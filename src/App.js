63f40c477c673d61f62ed44f47330c35ac99a2cb
Luis Salazar <luissa@lagash.com>
2020-01-21 13:01:36 -0300

    New pages and routes conditional

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from 'react'
 import { Router } from '@reach/router'
 import { Logo } from './components/Logo'
 import { NavBar } from './components/NavBar'
+import { UserLogged } from './components/UserLogged'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisterUser } from './pages/NotRegisterUser'
 import { PhotoCardWithQuery } from './containers/PhotoCardWithQuery'
 
 export const App = () => {
@@ -17,11 +21,29 @@ export const App = () => {
         detailId
           ? <PhotoCardWithQuery id={detailId} />
           : (
-            <Router>
-              <Home path='/' />
-              <Home path='/pet/:id' />
-              <Detail path='/detail/:detailId' />
-            </Router>
+            <>
+              <Router>
+                <Home path='/' />
+                <Home path='/pet/:id' />
+                <Detail path='/detail/:detailId' />
+              </Router>
+              <UserLogged>
+                {
+                  ({ isAuth }) =>
+                    isAuth ? (
+                      <Router>
+                        <Favs path='/favs' />
+                        <User path='/user' />
+                      </Router>
+                    ) : (
+                      <Router>
+                        <NotRegisterUser path='/favs' />
+                        <NotRegisterUser path='/user' />
+                      </Router>
+                    )
+                }
+              </UserLogged>
+            </>
           )
       }
       <NavBar />
---
+++ src/pages/NotRegisterUser.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const NotRegisterUser = () => {
+  return (
+    <div>
+      <h1>NotRegisterUser</h1>
+    </div>
+  )
+}
+
+NotRegisterUser.displayName = 'NotRegisterUser'
